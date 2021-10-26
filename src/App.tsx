86d0bd4d91e0a1eb6aa55e164c5cc787523a6445
Yusuke-Kurihara-unicast <91720261+Yusuke-Kurihara-unicast@users.noreply.github.com>
2021-10-26 12:01:21 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+import { UserCard } from "./components/UserCard";
+import { useAllUsers } from "./hooks/useAllUsers";
+
+export default function App() {
+  const { getUsers, userProfiles, loading, error } = useAllUsers();
+
+  const onClickFetchUser = () => {
+    getUsers();
+  };
+
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>error</p>
+      ) : loading ? (
+        <p>Now Loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
+    </div>
+  );
+}
